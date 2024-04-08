8e81a1af75bd7345ecf22890fa1d300ac834649c
Jonas <publik.froeller.jonas@gmx.at>
2024-04-08 11:24:36 +0200

    Update Configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// We're using the singleton here for convenience:
+// import { exiftool } from "exiftool-vendored"; // no esm support and @angular-builders/custom-webpack is not working properly (no :application)
+
+// And to verify everything is working:
+// exiftool
+//  .version()
+//  .then((version: any) => console.log(`We're running ExifTool v${version}`))
+
 @Component({
   selector: 'app-root',
   standalone: true,
